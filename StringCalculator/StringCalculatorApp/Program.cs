2431ec06bb5a05e300bf355ecfda494ec14e10b5
John Nooney <1803534@uad.ac.uk>
2024-08-18 22:29:31 +0100

    Add main program loop

--- StringCalculator/StringCalculatorApp/Program.cs
+++ StringCalculator/StringCalculatorApp/Program.cs
@@ -7,10 +7,35 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             StringCalculator calculator = new StringCalculator();
-            calculator.Add("");
+
+            Console.WriteLine("Welcome to the string calculator app! ");
+            Console.WriteLine("You can add together any sequence of numbers. Just follow these rules:");
+            Console.WriteLine("- default delimiters: \\n and ,");
+            Console.WriteLine("- create your own delimiters by following this format: //[delimiter]\\n<your number sequence>");
+            Console.WriteLine("   - multiple delimiters by following this format: //[delimiter][delimiter]\\n<your number sequence>");
+            Console.WriteLine("   - valid examples: //|\\n1|2|3  , //[|][%]\\n1|2%3");
+
+            while (true)
+            {
+                
+                Console.Write("\nEnter your sequence: ");
+                string input;
+                try
+                {
+                    input = Console.ReadLine();
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Error reading from console: " + e.Message);
+                    // Handle the error, e.g., retry, log, exit
+                    return;
+                }
+
+                int sum = calculator.Add(input);
+
+                Console.WriteLine("SUM = " + sum);
+            }
         }
     }
 }
